ba19a6e1f6959e860c7bb3316611751228b88056
Zalewa <zalewski.ba@gmail.com>
2025-03-23 14:48:41 +0100

    Change EditSet scene

---
+++ flashcards/src/main/java/flashcards_p/EditSetController.java
@@ -0,0 +1,68 @@
+package flashcards_p;
+
+import javafx.fxml.FXML;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.sql.SQLException;
+
+public class EditSetController {
+
+    @FXML
+    private ScrollPane mainPane;
+    @FXML
+    private TextField leftField;
+    @FXML
+    private TextField rightField;
+    @FXML
+    private VBox mainVBox;
+
+    InsertData insertData = new InsertData();
+    GetData getData = new GetData();
+
+
+    @FXML
+    protected void onAddButtonClick() {
+        insertData.insert(leftField.getText(), rightField.getText());
+    }
+
+    @FXML
+    protected void initialize() throws SQLException {
+        String[][] ar = getData.get();
+        mainVBox.getChildren().clear();
+
+        for(int i = 0; i<ar[0].length; i++) {
+            mainPane.setFitToWidth(true);
+            HBox row = new HBox();
+            mainVBox.getChildren().add(row);
+
+            Label plLabel = new Label(ar[0][i]);
+            plLabel.prefWidthProperty().bind(row.widthProperty().divide(2).subtract(40));
+            plLabel.setAlignment(Pos.CENTER);
+            row.getChildren().add(plLabel);
+
+            row.getChildren().add(new Separator(Orientation.VERTICAL));
+
+            Label enLabel = new Label(ar[1][i]);
+            enLabel.setAlignment(Pos.CENTER);
+            enLabel.prefWidthProperty().bind(row.widthProperty().divide(2).subtract(40));
+            row.getChildren().add(enLabel);
+
+            Button deleteButton = new Button("Delete");
+            //deleteButton.setOnAction(event -> {});
+            deleteButton.prefWidthProperty().set(60);
+            row.getChildren().add(deleteButton);
+            /*
+            row.getChildren().add(new Label(ar[0][i]));
+            row.getChildren().add(new Separator(Orientation.VERTICAL));
+            row.getChildren().add(new Label(ar[1][i]));
+            row.getChildren().add(new Separator(Orientation.VERTICAL));
+            row.getChildren().add(new Button("Delete"));*/
+
+            mainVBox.getChildren().add(new Separator(Orientation.HORIZONTAL));
+        }
+    }
+}
--- flashcards/src/main/java/flashcards_p/Main.java
+++ flashcards/src/main/java/flashcards_p/Main.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        //FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("add-show-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("edit-set-view.fxml"));
         //FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("test-view.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("MAIN");
         stage.setResizable(false);
